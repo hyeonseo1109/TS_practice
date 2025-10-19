ecd50bf2bdde2b985009481d9734f8b53fe8517a
hyeonseo1109 <alsgustj2@naver.com>
2025-10-20 03:39:55 +0900

    queryClient 연결

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,21 @@ import DropDownPage from "./pages/DropDownPage";
 import CardsPage from "./pages/CardsPage";
 import ToastPage from "./pages/ToastPage";
 import ToastContainer from "./commonComponent/toast/ToastContainer";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./api/queryClient";
 
 function App() {
   return (
     <>
-      <ToastContainer />
-      <Routes>
-        <Route path="/mapPractice" element={<MapPractice />} />
-        <Route path="/dropDownPage" element={<DropDownPage />} />
-        <Route path="/" element={<CardsPage />} />
-        <Route path="/toastPage" element={<ToastPage />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <Routes>
+          <Route path="/mapPractice" element={<MapPractice />} />
+          <Route path="/dropDownPage" element={<DropDownPage />} />
+          <Route path="/" element={<CardsPage />} />
+          <Route path="/toastPage" element={<ToastPage />} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
